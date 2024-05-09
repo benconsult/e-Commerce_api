793b8c8d90da9c14968ddcfd790bbfd271138642
benconsult <bendayo38@gmail.com>
2024-05-09 04:58:24 +0100

    404 vs not found middleware

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
+//avoid try-catch manually
+require('express-async-errors')
 
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
 
 app.get('/', (req,res)=>{
-    res.send('e-commerce apo')
+    res.send('e-commerce api')
 })
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);//should be last
 
 const port = process.env.PORT || 3000;
 const start = async () =>{
