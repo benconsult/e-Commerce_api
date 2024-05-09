c291da873ebcd83f4394cb8de6a1a086c4b8f896
benconsult <bendayo38@gmail.com>
2024-05-09 06:15:12 +0100

    email validator

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const UserSchema = new mongoose.Schema({
  name: {
@@ -11,6 +12,10 @@ const UserSchema = new mongoose.Schema({
  email: {
     type: String,
     required: [true,'Please provide email'],
+    validate: {
+        validator: validator.isEmail,
+        message:'Please provide valid email'
+    }
    
  },
  password: {
