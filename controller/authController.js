0094a9c267fe9bbae18e5133a232b663a2bc13e5
benconsult <bendayo38@gmail.com>
2024-05-18 15:20:10 +0100

    refactor cookies setup

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 const User = require('../model/user')
 const {StatusCodes} = require('http-status-codes')
 const CustomError = require('../errors')
-const {createJWT} = require('../utils')
+const {attachCookiesToResponse} = require('../utils')
 
 const register = async (req,res)=>{
     const {name, email,password} = req.body;
@@ -15,12 +15,9 @@ const register = async (req,res)=>{
     const user = await User.create({name,email,password,role});
     //generate token from payload 
     const tokenUser = { name:user.name, userId:user._id, role:user.role };
-    const token = createJWT({ payload:tokenUser })
-
-    //passing with cookie
-    const oneDay = 1000 * 60 * 60 * 24
-    res.cookie('token',token, { httpOnly:true, expires: new Date(Date.now() + oneDay)});
-
+    // already in use in the utils - jwt.js const token = createJWT({ payload:tokenUser })
+    attachCookiesToResponse({res, user:tokenUser});
+    
     res.status(StatusCodes.CREATED).json({ user: tokenUser });
 }
 const login = async (req,res)=>{
--- utils/jwt.js
+++ utils/jwt.js
@@ -9,4 +9,12 @@ return token;
 
 const isTokenValid = ({ token})=> jwt.verify(token, process.env.JWT_SECRET);
 
-module.exports = { createJWT, isTokenValid}
+//cookies
+const attachCookiesToResponse = ({ res, user})=>{
+    const token = createJWT({ payload:user});
+    //passing with cookie
+    const oneDay = 1000 * 60 * 60 * 24
+    res.cookie('token',token, { httpOnly:true, expires: new Date(Date.now() + oneDay)});
+    //res.status(201).json({ user });
+}
+module.exports = { createJWT, isTokenValid, attachCookiesToResponse}
